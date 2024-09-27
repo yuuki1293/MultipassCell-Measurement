4cc4d50d134bfc25de8849c171cf54a8bd5e8992
yuuki1293 <yuuki1293@outlook.jp>
2024-09-27 09:29:01 +0900

    細い光が検出できない

--- main.cpp
+++ main.cpp
@@ -6,15 +6,27 @@ using namespace cv;
 using namespace std;
 
 int main(){
-    std::string image_path = "sample.png";
+    std::string image_path = "many_circle_bin.png";
     Mat src = imread(image_path, IMREAD_COLOR);
     Mat gray;
     cvtColor(src, gray, COLOR_BGR2GRAY);
     medianBlur(gray, gray, 5);
 
     vector<Vec3f> circles;
-    HoughCircles(src, circles, HOUGH_GRADIENT, 1, 0);
-    imshow("Display window", circles);
+    HoughCircles(gray, circles, HOUGH_GRADIENT, 1, 50, 100, 10, 5, 30);
+
+    for( size_t i = 0; i < circles.size(); i++ )
+    {
+        Vec3i c = circles[i];
+        Point center = Point(c[0], c[1]);
+        // circle center
+        circle( src, center, 1, Scalar(0,100,100), 3, LINE_AA);
+        // circle outline
+        int radius = c[2];
+        circle( src, center, radius, Scalar(255,0,255), 3, LINE_AA);
+    }
+
+    imshow("Display window", src);
     int k = waitKey(0);
     return 0;
 }
