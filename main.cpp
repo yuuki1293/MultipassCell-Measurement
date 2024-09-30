4fc6317f9a9dbad095c029c6425ed96f17899e22
yuuki1293 <yuuki1293@outlook.jp>
2024-09-30 10:38:18 +0900

    print real coordinates

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include<string>
 #include<opencv2/opencv.hpp>
 
+#define REAL_LEN 88900 // μm
+
 using namespace cv;
 using namespace std;
 using namespace traits;
@@ -17,31 +19,55 @@ int main(){
     cvtColor(src, gray, COLOR_BGR2GRAY);
     medianBlur(gray, gray, 5);
 
-    gray = remove_area(gray, 200, 2000);
-    gray = crop(gray, 400, 400, 400, 400);
-    vector<Vec3f> circles;
+    gray = remove_area(gray, 200, 2000); // 200~200ピクセル以外の図形を削除
+    gray = crop(gray, 400, 400, 400, 400); // 縁から400ピクセルを削除
     int n = connectedComponentsWithStats(gray, labelImg, stats, centroids);
     
+    Point centers[n];
     // 重心
     for (size_t i = 1; i < n; i++){
         double *param = centroids.ptr<double>(i);
-        int x = static_cast<int>(param[0]);
-        int y = static_cast<int>(param[1]);
+        int x = param[0];
+        int y = param[1];
 
+        centers[i] = Point(x, y);
         circle(Dst, Point(x, y), 3, Scalar(0, 0, 255), -1);
     }
 
     // 面積値の出力
     for (size_t i = 1; i < n; i++) {
         int *param = stats.ptr<int>(i);
-        std::print("area {} = {} ({}, {})\n", i, param[ConnectedComponentsTypes::CC_STAT_AREA], param[ConnectedComponentsTypes::CC_STAT_LEFT], param[ConnectedComponentsTypes::CC_STAT_TOP]);
-
         int x = param[ConnectedComponentsTypes::CC_STAT_LEFT];
         int y = param[ConnectedComponentsTypes::CC_STAT_TOP];
+
+        std::print("area {} = {} ({}, {})\n", i, param[ConnectedComponentsTypes::CC_STAT_AREA], centers[i].x, centers[i].y);
+
         stringstream num;
         num << i;
         putText(Dst, num.str(), cv::Point(x + 5, y + 10), FONT_HERSHEY_COMPLEX, 0.7, Scalar(255, 0, 0), 2);
     }
+
+    // 中心を（0, 0）にする
+    for (size_t i = 1; i < n; i++)
+    {
+        Point zero(src.cols / 2, src.rows / 2);
+
+        centers[i] = centers[i] - zero;
+    }
+    
+    // 単位をピクセルからμｍに変換
+    for (size_t i = 1; i < n; i++)
+    {
+        int um_per_pix = REAL_LEN / src.cols;
+
+        centers[i] = centers[i] * um_per_pix;
+    }
+    
+    for (size_t i = 1; i < n; i++)
+    {
+        print("{} = ({}, {})\n", i, centers[i].x, centers[i].y);
+    }
+    
 
     imshow("Display window", Dst);
     int k = waitKey(0);
