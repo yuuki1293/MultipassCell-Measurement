cba81dbf67dd4a76421b2668ced2dd6ffed3fef5
yuuki1293 <yuuki1293@outlook.jp>
2024-10-02 14:39:56 +0900

    SVGに保存

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
-#include<stdio.h>
-#include<string>
-#include<opencv2/opencv.hpp>
+#include <stdio.h>
+#include <string>
+#include <fstream>
+#include <opencv2/opencv.hpp>
 
 #define REAL_LEN 88900 // μm
 
@@ -10,8 +11,10 @@ using namespace traits;
 
 Mat remove_area(const Mat image, int min, int max);
 Mat crop(const Mat image, int left, int top, int right, int bottom);
+string toSVG(const Point *centers, int n);
 
-int main(){
+int main()
+{
     std::string image_path = "many_circle_bin.png";
     Mat src = imread(image_path, IMREAD_COLOR);
     Mat gray, labelImg, stats, centroids, Dst;
@@ -19,13 +22,14 @@ int main(){
     cvtColor(src, gray, COLOR_BGR2GRAY);
     medianBlur(gray, gray, 5);
 
-    gray = remove_area(gray, 200, 2000); // 200~200ピクセル以外の図形を削除
+    gray = remove_area(gray, 200, 2000);   // 200~200ピクセル以外の図形を削除
     gray = crop(gray, 400, 400, 400, 400); // 縁から400ピクセルを削除
     int n = connectedComponentsWithStats(gray, labelImg, stats, centroids);
-    
+
     Point centers[n];
     // 重心
-    for (size_t i = 1; i < n; i++){
+    for (size_t i = 1; i < n; i++)
+    {
         double *param = centroids.ptr<double>(i);
         int x = param[0];
         int y = param[1];
@@ -35,7 +39,8 @@ int main(){
     }
 
     // 面積値の出力
-    for (size_t i = 1; i < n; i++) {
+    for (size_t i = 1; i < n; i++)
+    {
         int *param = stats.ptr<int>(i);
         int x = param[ConnectedComponentsTypes::CC_STAT_LEFT];
         int y = param[ConnectedComponentsTypes::CC_STAT_TOP];
@@ -47,14 +52,14 @@ int main(){
         putText(Dst, num.str(), cv::Point(x + 5, y + 10), FONT_HERSHEY_COMPLEX, 0.7, Scalar(255, 0, 0), 2);
     }
 
-    // 中心を（0, 0）にする
-    for (size_t i = 1; i < n; i++)
-    {
-        Point zero(src.cols / 2, src.rows / 2);
+    // // 中心を（0, 0）にする
+    // for (size_t i = 1; i < n; i++)
+    // {
+    //     Point zero(src.cols / 2, src.rows / 2);
 
-        centers[i] = centers[i] - zero;
-    }
-    
+    //     centers[i] = centers[i] - zero;
+    // }
+
     // 単位をピクセルからμｍに変換
     for (size_t i = 1; i < n; i++)
     {
@@ -62,12 +67,16 @@ int main(){
 
         centers[i] = centers[i] * um_per_pix;
     }
-    
+
+    // Save svg
+    std::ofstream out("out.svg");
+    out << toSVG(centers, n);
+    out.close();
+
     for (size_t i = 1; i < n; i++)
     {
         print("{} = ({}, {})\n", i, centers[i].x, centers[i].y);
     }
-    
 
     imshow("Display window", Dst);
     int k = waitKey(0);
@@ -75,7 +84,8 @@ int main(){
 }
 
 // 特定の大きさのオブジェクト以外を削除
-Mat remove_area(const Mat image, int min, int max){
+Mat remove_area(const Mat image, int min, int max)
+{
     Mat hierarchy, result;
     vector<vector<Point>> contours;
     image.copyTo(result);
@@ -85,16 +95,18 @@ Mat remove_area(const Mat image, int min, int max){
     for (size_t i = 0; i < contours.size(); i++)
     {
         double area = contourArea(contours[i]);
-        if(!(min <= area && area <= max)){
+        if (!(min <= area && area <= max))
+        {
             drawContours(result, contours, (int)i, Scalar(0), -1);
         }
     }
-    
+
     return result;
 }
 
 // 端を黒塗りする
-Mat crop(const Mat image, int left, int top, int right, int bottom){
+Mat crop(const Mat image, int left, int top, int right, int bottom)
+{
     Mat result;
     image.copyTo(result);
 
@@ -102,6 +114,21 @@ Mat crop(const Mat image, int left, int top, int right, int bottom){
     rectangle(result, Point(0, 0), Point(result.cols, top), Vec3b(0, 0, 0), FILLED);
     rectangle(result, Point(result.cols - right, 0), Point(result.cols, result.rows), Vec3b(0, 0, 0), FILLED);
     rectangle(result, Point(0, result.rows - bottom), Point(result.cols, result.rows), Vec3b(0, 0, 0), FILLED);
-    
+
     return result;
+}
+
+string toSVG(const Point *centers, int n)
+{
+    string s = "<?xml version=\"1.0\"?>";
+    s += "<svg width=\"88.9mm\" height=\"88.9mm\" xmlns=\"http://www.w3.org/2000/svg\">";
+
+    for (size_t i = 1; i < n; i++)
+    {
+        s += std::format("<circle cx=\"{}mm\" cy=\"{}mm\" r=\"0.5mm\" stroke=\"black\" fill=\"#fff\" stroke-width=\"1\" />", centers[i].x / 1000., centers[i].y / 1000.);
+    }
+
+    s += "</svg>";
+
+    return s;
 }
